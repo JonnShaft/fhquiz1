1959147768d978b7ef4c487e35e175617d795e52
Ahmad Jamal <akj824@gmail.com>
2015-06-21 04:05:39 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fhquiz1::Application.config.secret_key_base = '75faed0ded4f1825a80b85bbe64cdfbd989a9708b0451bc69678ac9bd280b146b3d8c258bc4847331f9baebf6f8156cb0b614a1e5d004369fe3ba4e08bd36854'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fhquiz1::Application.config.session_store :cookie_store, key: '_fhquiz1_session'
